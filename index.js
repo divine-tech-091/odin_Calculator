584314e4c72761655d052aebec7c470bdb5eda07
diitech <123456789+odinusers.noreply.github.com>
2026-08-17 08:34:38 +0100

    Added title to page and created functions. - created function for each operation. - created three variables for each operation. - added title to my page.

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(one, two) {
+	return one + two;
+}
+console.log(add(1, 2));
+
+function subtract(num, val) {
+	return val - num;
+}
+console.log(subtract(2, 3));
+
+function multiply(four, two) {
+	return four * two;
+}
+console.log(multiply(4, 2));
+
+function divide(six, three) {
+	return three / six;
+}
+console.log(divide(3, 6));
+
+let additionOperator = 3 + 5;
+let subtractOperator = 4 - 3;
+let divideOperator = 4 / 2;
