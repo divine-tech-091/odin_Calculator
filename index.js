194b2711bf6bdd41d0e54a62317c3a5f981c53bb
diitech <123456789+odinusers.noreply.github.com>
2026-09-15 16:11:07 +0100

    completed the project functionality. - Renamed the attributes of the html btns. - write the functionality of the javascript. -add opacity to the btns on css file, and increased  the font-size and padding of the calc display.

--- index.js
+++ index.js
@@ -1,23 +1,229 @@
 function add(one, two) {
 	return one + two;
 }
-console.log(add(1, 2));
 
 function subtract(num, val) {
-	return val - num;
+	return num - val;
 }
-console.log(subtract(2, 3));
 
 function multiply(four, two) {
 	return four * two;
 }
-console.log(multiply(4, 2));
 
 function divide(six, three) {
-	return three / six;
+	return six / three;
 }
-console.log(divide(3, 6));
 
-let additionOperator = undefined;
-let subtractOperator = undefined;
-let divideOperator = undefined;
+function remainder(num1, num2) {
+	return num1 % num2;
+}
+console.log(remainder(3, 4));
+
+let currentNumber = '0';
+let operator = '';
+let previousNumber = '';
+
+const displayText = document.querySelector('#text_Input');
+const btnNums = document.querySelectorAll('.num');
+const operatorBtn = document.querySelectorAll('#operatorBtn');
+const equalBtn = document.querySelector('#equal');
+const clearBtn = document.querySelector('#clear_All');
+const backSpaceBtn = document.querySelector('#clear');
+
+let result = '';
+let addOpera = '';
+
+displayText.value = '0';
+
+console.log(operatorBtn);
+function chooseOperator(event) {
+	operator = event.target.innerText;
+
+	if (addOpera === '') {
+		addOpera = operator;
+	}
+	if (previousNumber === '') {
+		previousNumber = currentNumber;
+		currentNumber = '';
+	}
+	if (addOpera === 'x' && currentNumber !== '') {
+		const previous = parseFloat(previousNumber);
+		const current = parseFloat(currentNumber);
+
+		result = operate(addOpera, previous, current);
+		currentNumber = result;
+		addOpera = '';
+		previousNumber = currentNumber;
+		currentNumber = '';
+		updateValue(result);
+	}
+
+	if (addOpera === '-' && currentNumber !== '') {
+		const previous = parseFloat(previousNumber);
+		const current = parseFloat(currentNumber);
+
+		result = operate(addOpera, previous, current);
+		currentNumber = result;
+		addOpera = '';
+		previousNumber = currentNumber;
+		currentNumber = '';
+		updateValue(result);
+	}
+
+	if (addOpera === '/' && currentNumber !== '') {
+		const previous = parseFloat(previousNumber);
+		const current = parseFloat(currentNumber);
+
+		result = operate(addOpera, previous, current);
+		currentNumber = result;
+		addOpera = '';
+		previousNumber = currentNumber;
+		currentNumber = '';
+		updateValue(result);
+	}
+
+	if (addOpera === '+' && currentNumber !== '') {
+		const previous = parseFloat(previousNumber);
+		const current = parseFloat(currentNumber);
+
+		result = operate(addOpera, previous, current);
+		currentNumber = result;
+		addOpera = '';
+		previousNumber = currentNumber;
+		currentNumber = '';
+		updateValue(result);
+	}
+}
+
+function operate(isOperator, firstNum, secondNum) {
+	if (isOperator === '+') {
+		return add(firstNum, secondNum);
+	}
+	if (isOperator === 'x') {
+		return multiply(firstNum, secondNum);
+	}
+	if (isOperator === '/') {
+		return divide(firstNum, secondNum);
+	}
+	if (isOperator === '-') {
+		return subtract(firstNum, secondNum);
+	}
+
+	if (isOperator === '%') {
+		return remainder(firstNum, secondNum);
+	}
+}
+
+function updateValue(values) {
+	displayText.value = values;
+}
+
+function updateNumber(event) {
+	let btnText = event.target.innerText;
+	if (currentNumber === '0') {
+		currentNumber = '';
+	}
+	currentNumber += btnText;
+	updateValue(currentNumber);
+}
+
+btnNums.forEach((clickNums) => {
+	clickNums.addEventListener('click', updateNumber);
+});
+
+operatorBtn.forEach((clickOperator) => {
+	clickOperator.addEventListener('click', chooseOperator);
+});
+
+equalBtn.addEventListener('click', () => {
+	const prev = parseFloat(previousNumber);
+	const curr = parseFloat(currentNumber);
+
+	if (!isNaN(prev) && !isNaN(curr)) {
+		switch (operator) {
+			case '+':
+				result = operate(operator, prev, curr);
+				currentNumber = result;
+				operator = '';
+				previousNumber = currentNumber;
+				currentNumber = '';
+				updateValue(result);
+				previousNumber = '';
+				break;
+			case '-':
+				result = operate(operator, prev, curr);
+				currentNumber = result;
+				operator = '';
+				previousNumber = currentNumber;
+				currentNumber = '';
+				updateValue(result);
+				previousNumber;
+				break;
+			case '%':
+				result = operate(operator, prev, curr);
+				currentNumber = result;
+				operator = '';
+				previousNumber = currentNumber;
+				currentNumber = '';
+				updateValue(result);
+				// result = '';
+				previousNumber;
+				break;
+			case 'x':
+				result = operate(operator, prev, curr);
+				currentNumber = result;
+				operator = '';
+				previousNumber = currentNumber;
+				currentNumber = '';
+				updateValue(result);
+				previousNumber = '';
+				break;
+			case '/':
+				switch (true) {
+					case curr === 0 || prev === 0:
+						let isError = 'Error';
+						result = isError;
+						updateValue(result);
+						break;
+				}
+				break;
+				result = operate(operator, prev, curr);
+				currentNumber = result;
+				operator = '';
+				previousNumber = currentNumber;
+				currentNumber = '';
+				updateValue(result);
+				previousNumber = '';
+				break;
+			default:
+				break;
+		}
+	}
+});
+
+clearBtn.addEventListener('click', () => {
+	currentNumber = '0';
+	operator = '';
+	previousNumber = '';
+	result = 0;
+	updateValue(currentNumber);
+});
+
+backSpaceBtn.addEventListener('click', () => {
+	if (previousNumber === result) {
+		currentNumber = result;
+		let convertResultToString = String(currentNumber);
+		// console.log(convertResultToString);
+		let currentLength = convertResultToString.length;
+		currentNumber = currentNumber.slice(0, currentLength - 1);
+	}
+
+	let getLengthOfDisplay = currentNumber.length;
+	currentNumber = currentNumber.slice(0, getLengthOfDisplay - 1);
+
+	if (currentNumber === '') {
+		currentNumber = '0';
+	}
+
+	updateValue(currentNumber);
+});
